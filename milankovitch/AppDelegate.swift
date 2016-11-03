105a95c942da8f791ae165ddeb3d5a7347a32ac1
Julio César Melchor Pinto <jmelchorp@gmail.com>
2016-11-02 21:38:36 -0600

    Initial Commit

---
+++ milankovitch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  milankovitch
+//
+//  Created by Víctor Mendoza on 02/11/16.
+//  Copyright © 2016 UNAM. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ milankovitch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  milankovitch
+//
+//  Created by Víctor Mendoza on 02/11/16.
+//  Copyright © 2016 UNAM. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
